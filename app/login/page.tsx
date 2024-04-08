d8a0e4ceae6ae58736d3f3ef9ba86b5f04e2d424
Supun Sudaraka <ssudaraka@gmail.com>
2024-04-08 16:54:53 +0530

    added saml auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -51,6 +51,25 @@ export default function Login({
     return redirect("/login?message=Check email to continue sign in process");
   };
 
+  const signInWithSAML = async () => {
+    "use server";
+    const origin = headers().get("origin");
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInWithSSO({
+      domain: "sudaraka.com",
+      options: {
+        redirectTo: `${origin}/auth/callback`,
+      },
+    });
+
+    console.log(data);
+    console.log(error);
+
+    if (data?.url) {
+      return redirect(data.url);
+    }
+  };
+
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
@@ -75,44 +94,12 @@ export default function Login({
       </Link>
 
       <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
+        <button
+          formAction={signInWithSAML}
           className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
         >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
+          Sign In with SAML
+        </button>
       </form>
     </div>
   );
